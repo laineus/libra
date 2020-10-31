b0d5f477165bf38dd3ee2f8c0c4f9d81d480fa90
laineus <lainen000@gmail.com>
2020-10-31 10:48:59 +0900

    use substance for character

--- src/components/Substance.vue
+++ src/components/Substance.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Container ref="object" :x="initX" :y="initY" :width="imgWidth" :height="imgWidth" :depth="initY" @preUpdate="update">
+    <Container ref="object" :x="initX" :y="initY" :width="imgWidth" :height="imgWidth" :depth="depth" @create="create" @preUpdate="update">
       <Image ref="image" :texture="`chara_sprite/${name}`" :originX="0.5" :originY="1" v-if="name" />
     </Container>
     <TapArea v-if="tapEvent.event.value" :visible="checkable" :width="imgWidth + 15" :height="imgHeight + 40" :follow="object" @tap="tapEvent.exec" />
@@ -9,7 +9,7 @@
 
 <script>
 import { refObj, Container, Image } from 'phavuer'
-import { computed, inject, reactive, toRefs } from 'vue'
+import { computed, inject, reactive, ref, toRefs } from 'vue'
 import TapArea from './TapArea'
 import useEvent from './modules/useEvent'
 export default {
@@ -19,7 +19,7 @@ export default {
     initY: { default: 0 },
     name: { default: null }
   },
-  emits: ['preUpdate'],
+  emits: ['create', 'preUpdate'],
   setup (props, context) {
     const event = inject('event')
     const player = inject('player')
@@ -27,20 +27,23 @@ export default {
     const image = refObj(null)
     const imgWidth = computed(() => image.value ? image.value.width : 30)
     const imgHeight = computed(() => image.value ? image.value.height : 30)
+    const depth = ref(0)
     const tapEvent = useEvent()
     const data = reactive({
       distanceToPlayer: null
     })
+    const create = obj => context.emit('create', obj)
     const update = obj => {
+      if (depth.value !== obj.y) depth.value = obj.y
       data.distanceToPlayer = Phaser.Math.Distance.Between(object.value.x, object.value.y, player.value.object.x, player.value.object.y)
       context.emit('preUpdate', obj)
     }
     return {
       ...toRefs(data),
       checkable: computed(() => !event.state && tapEvent.event.value && data.distanceToPlayer < 150),
-      update,
+      create, update,
       object, image,
-      imgWidth, imgHeight,
+      imgWidth, imgHeight, depth,
       tapEvent, setTapEvent: tapEvent.setEvent
     }
   }
--- src/components/modules/useFrameAnimChara.js
+++ src/components/modules/useFrameAnimChara.js
@@ -1,2 +1,34 @@
 import useFrameAnim from '@/components/modules/useFrameAnim'
-
+import { inject, unref } from 'vue'
+const WALK_ANIM = [
+  { key: 'down', frames: [1, 0, 1, 2], duration: 7 },
+  { key: 'left', frames: [4, 3, 4, 5], duration: 7 },
+  { key: 'right', frames: [7, 6, 7, 8], duration: 7 },
+  { key: 'up', frames: [10, 9, 10, 11], duration: 7 }
+]
+const BASE_FRAME = { down: 1, left: 4, right: 7, up: 10 }
+const velocityToDirectionKey = (x, y) => {
+  if (Math.round(Math.abs(x)) > Math.round(Math.abs(y))) return x < 0 ? 'left' : 'right'
+  return y < 0 ? 'up' : 'down'
+}
+export default (object, image, initR) => {
+  const event = inject('event')
+  const frameAnim = useFrameAnim(WALK_ANIM, image)
+  let directionKey = velocityToDirectionKey(Math.cos(initR), Math.sin(initR))
+  const play = () => {
+    if (event.state) {
+      unref(image).setFrame(BASE_FRAME[directionKey])
+      return
+    }
+    const walking = Math.hypot(unref(object).body.velocity.x, unref(object).body.velocity.y) > 1
+    if (walking) {
+      directionKey = velocityToDirectionKey(unref(object).body.velocity.x, unref(object).body.velocity.y)
+      frameAnim.play(directionKey)
+    } else {
+      unref(image).setFrame(BASE_FRAME[directionKey]) // Set last frame
+    }
+  }
+  return {
+    play
+  }
+}
