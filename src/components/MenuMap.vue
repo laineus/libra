abd308afc3256155771500744a8c4e5a3d9911fe
laineus <lainen000@gmail.com>
2020-11-07 01:31:56 +0900

    move

--- src/components/MenuMap.vue
+++ src/components/MenuMap.vue
@@ -1,7 +1,7 @@
 <template>
   <MenuContainer :arrowX="25 + (2 * 60)" :height="320" :title="'Map'">
     <Container v-for="(v, i) in places" :key="i" :y="i * 30">
-      <Text :x="10" :y="8" :text="`${v.key} x: ${v.x} y: ${v.y}`" :style="{ fontSize: 13, fontStyle: 'bold', color: '#553311' }" />
+      <Text :x="10" :y="8" :text="`${v.key} x: ${v.x} y: ${v.y}`" :style="{ fontSize: 13, fontStyle: 'bold', color: '#553311' }" @pointerdown="onTap(v)" />
     </Container>
   </MenuContainer>
 </template>
@@ -13,10 +13,14 @@ import { Container, Text } from 'phavuer'
 export default {
   components: { MenuContainer, Container, Text },
   setup () {
+    const gameScene = inject('gameScene').value
     const storage = inject('storage')
     const places = storage.state.places
     return {
-      places
+      places,
+      onTap: (place) => {
+        gameScene.setField(place.key, place.x, place.y)
+      }
     }
   }
 }
