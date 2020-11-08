eb6b173fc25ab75b906041451152c75a1a0d7c8f
laineus <lainen000@gmail.com>
2020-11-09 02:03:53 +0900

    selector for menuMap

--- src/components/MenuMap.vue
+++ src/components/MenuMap.vue
@@ -3,23 +3,35 @@
     <Container v-for="(v, i) in places" :key="i" :y="i * 30">
       <Text :x="10" :y="8" :text="`${v.key} x: ${v.x} y: ${v.y}`" :style="{ fontSize: 13, fontStyle: 'bold', color: '#553311' }" @pointerdown="onTap(v)" />
     </Container>
+    <Selector v-if="selected" :x="20" :y="70" :list="['へ移動', 'キャンセル']" @select="submit" />
   </MenuContainer>
 </template>
 
 <script>
 import MenuContainer from '@/components/MenuContainer'
-import { inject } from 'vue'
+import { inject, ref } from 'vue'
 import { Container, Text } from 'phavuer'
+import Selector from './Selector'
 export default {
-  components: { MenuContainer, Container, Text },
-  setup () {
+  components: { MenuContainer, Container, Text, Selector },
+  emits: ['close'],
+  setup (_, context) {
     const gameScene = inject('gameScene').value
     const storage = inject('storage')
     const places = storage.state.places
+    const selected = ref(null)
+    const submit = i => {
+      if (i === 1) return selected.value = null
+      const place = places[i]
+      gameScene.setField(place.key, place.x, place.y)
+      context.emit('close')
+    }
     return {
       places,
+      selected,
+      submit,
       onTap: (place) => {
-        gameScene.setField(place.key, place.x, place.y)
+        selected.value = place
       }
     }
   }
