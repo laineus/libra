d4766030c04f45d6dc0cc855c69be4667ca78fcb
laineus <lainen000@gmail.com>
2020-11-08 12:19:51 +0900

    selector

---
+++ src/components/Selector.vue
@@ -0,0 +1,43 @@
+<template>
+  <SpeachBubble :width="bgWidth" :height="(bgHeight * options.length) + (2 * (options.length - 1))">
+    <Container v-for="(v, i) in options" :key="i" :y="(bgHeight + 2) * i">
+      <RoundRectangle :fillColor="0x553311" :radius="4" :width="bgWidth" :height="bgHeight" @pointerdown="select(i)" />
+      <Rectangle :fillColor="0xBBAA88" :x="10" :y="14" :width="3" :height="3" :rotation="Math.PI / 4" />
+      <Text :ref="v.ref" :text="v.text" :style="{ fontSize: 14, fontStyle: 'normal', color: '#BBAA88' }" :x="18" :y="6" :lineSpacing="3" :padding="{ top: 2 }" />
+    </Container>
+  </SpeachBubble>
+</template>
+
+<script>
+import { refObj, Container, RoundRectangle, Rectangle, Text } from 'phavuer'
+import { reactive, toRefs, onMounted } from 'vue'
+import SpeachBubble from './SpeachBubble'
+import config from '@/data/config'
+export default {
+  components: { SpeachBubble, Container, RoundRectangle, Rectangle, Text },
+  props: ['list'],
+  emits: ['select'],
+  setup (props, context) {
+    // data
+    const options = props.list.map(text => {
+      return { ref: refObj(null), text }
+    })
+    const data = reactive({
+      bgWidth: 0,
+      bgHeight: 27
+    })
+    onMounted(() => {
+      data.bgWidth = Math.max(...options.map(v => v.ref.value.width)) + 32
+    })
+    const select = (i) => {
+      context.emit('select', i)
+    }
+    return {
+      config,
+      options,
+      ...toRefs(data),
+      select
+    }
+  }
+}
+</script>
