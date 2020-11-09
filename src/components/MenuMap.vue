83b07ecbc6fd5e4bb15f51fc3e1680e5ea0a6cc3
laineus <lainen000@gmail.com>
2020-11-10 00:52:57 +0900

    map

--- src/components/MenuMap.vue
+++ src/components/MenuMap.vue
@@ -1,15 +1,15 @@
 <template>
-  <MenuContainer :arrowX="25 + (2 * 60)" :height="320" :title="'Map'">
+  <MenuContainer ref="container" :arrowX="25 + (2 * 60)" :height="320" :title="'Map'">
     <Container v-for="(v, i) in places" :key="i" :y="i * 30">
-      <Text :x="10" :y="8" :text="`${v.key} x: ${v.x} y: ${v.y}`" :style="{ fontSize: 13, fontStyle: 'bold', color: '#553311' }" @pointerdown="onTap(v)" />
+      <Text :x="10" :y="8" :text="`${v.key} x: ${v.x} y: ${v.y}`" :style="{ fontSize: 13, fontStyle: 'bold', color: '#553311' }" @pointerdown="p => onTap(p, v)" />
     </Container>
-    <Selector v-if="selected" :x="20" :y="70" :list="['へ移動', 'キャンセル']" @select="submit" />
+    <Selector v-if="data.selected" :x="data.tapX" :y="data.tapY" :list="['へ移動', 'キャンセル']" @select="submit" />
   </MenuContainer>
 </template>
 
 <script>
 import MenuContainer from '@/components/MenuContainer'
-import { inject, ref } from 'vue'
+import { inject, reactive, ref } from 'vue'
 import { Container, Text } from 'phavuer'
 import Selector from './Selector'
 export default {
@@ -18,20 +18,31 @@ export default {
   setup (_, context) {
     const gameScene = inject('gameScene').value
     const storage = inject('storage')
+    const container = ref(null)
     const places = storage.state.places
-    const selected = ref(null)
+    const data = reactive({
+      selected: null,
+      tapX: 0, tapY: 0
+    })
     const submit = i => {
-      if (i === 1) return selected.value = null
+      if (i === 1) return data.selected = null
       const place = places[i]
       gameScene.setField(place.key, place.x, place.y)
       context.emit('close')
     }
     return {
       places,
-      selected,
+      container,
+      data,
       submit,
-      onTap: (place) => {
-        selected.value = place
+      onTap: (pointer, place) => {
+        if (data.selected) {
+          data.selected = null
+          return
+        }
+        data.tapX = pointer.x - container.value.offsetX + 5
+        data.tapY = pointer.y - container.value.offsetY - 10
+        data.selected = place
       }
     }
   }
