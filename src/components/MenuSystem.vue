412169a7cd8d37eba7c0e358b59631668376b87c
lainues <lainen000@gmail.com>
2020-11-13 22:12:22 +0900

    save

--- src/components/MenuSystem.vue
+++ src/components/MenuSystem.vue
@@ -1,11 +1,13 @@
 <template>
-  <MenuContainer ref="container" :arrowX="25 + (3 * 60)" :height="324" :title="'System'">
+  <MenuContainer ref="container" :arrowX="25 + (3 * 60)" :height="342" :title="'System'">
     <Container v-for="(v, i) in list" :key="i" :x="rowWidth.half" :y="(i * rowHeight) + rowHeight.half" :width="rowWidth" :height="rowHeight" @pointerdown="p => onTap(p, i)">
       <Rectangle :visible="i === selectedIndex" :fillColor="COLORS.orange" :width="rowWidth" :height="rowHeight" :alpha="0.8" />
       <Line v-if="i !== list.length - 1" :x="0" :y="rowHeight.half - 0.5" :lineWidth="0.5" :x2="rowWidth" :strokeColor="COLORS.brown" :alpha="0.25" />
       <Text :x="-rowWidth.half + 10" :y="0" :originY="0.5" :text="v.name" :style="{ fontSize: 13, fontStyle: 'bold', color: COLORS.brown.toColorString }" />
+      <Text v-if="v.exists" :x="-15" :y="0" :originY="0.5" :text="`${v.state.map} ${v.state.sec}`" :style="{ fontSize: 11, color: COLORS.brown.toColorString }" />
+      <Text v-if="i === 0" :x="-15" :y="0" :originY="0.5" :text="'交差点\n2020-11-13 10:23'" :lineSpacing="1" :style="{ fontSize: 11, color: COLORS.brown.toColorString }" />
     </Container>
-    <Selector v-if="selectedIndex !== null" :x="tapX" :y="tapY" :list="['保存', 'キャンセル']" @select="submit" />
+    <Selector v-if="selectedIndex !== null" :x="tapX" :y="tapY" :list="[list[selectedIndex].exists ? '上書き保存' : '保存', 'キャンセル']" @select="submit" />
   </MenuContainer>
 </template>
 
@@ -23,16 +25,22 @@ export default {
     const list = ref([])
     const container = ref(null)
     const data = reactive({
-      rowWidth: 220, rowHeight: 35,
+      rowWidth: 220, rowHeight: 37,
       selectedIndex: null,
       tapX: 0, tapY: 0
     })
-    sdm.getList().then(v => {
-      list.value = v
-      console.log(list.value)
-    })
+    const loadData = () => {
+      sdm.getList().then(v => {
+        list.value = v
+        console.log(list.value)
+      })
+    }
+    loadData()
     const submit = i => {
       if (i === 1) return data.selectedIndex = null
+      const row = list.value[data.selectedIndex]
+      sdm.save(row.number)
+      loadData()
     }
     return {
       COLORS: config.COLORS,
@@ -41,6 +49,7 @@ export default {
       ...toRefs(data),
       submit,
       onTap: (pointer, i) => {
+        if (!i) return
         if (data.selectedIndex) {
           data.selectedIndex = null
           return
