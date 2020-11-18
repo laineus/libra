541a35a89ca563deb50e88c5f263585d521d8c75
laineus <lainen000@gmail.com>
2020-11-18 23:10:05 +0900

    status libra

--- src/components/MenuStatus.vue
+++ src/components/MenuStatus.vue
@@ -1,25 +1,26 @@
 <template>
-  <MenuContainer :arrowX="25 + (0 * 64)" :height="320" :title="'Status'">
+  <MenuContainer :arrowX="25 + (0 * 64)" :height="350" :title="'Status'">
     <template v-for="(v, i) in LIST.slice(0, 3)" :key="i">
       <Text :x="10" :y="10 + (i * 20)" :text="v.label" :style="{ fontSize: 15, fontStyle: 'bold', color: COLORS.brown.toColorString }" />
       <Text :x="115" :y="10 + (i * 20)" :text="v.value.value" :style="{ fontSize: 15, fontStyle: 'bold', color: COLORS.brown.toColorString }" />
     </template>
+    <Image texture="libra" :tint="COLORS.brown" :x="115" :y="165" :frame="Math.abs(charmDiff)" :flipX="charmDiff < 0" />
     <template v-for="(v, i) in LIST.slice(3)" :key="i">
-      <Text :x="10" :y="240 + (i * 20)" :text="v.label" :style="{ fontSize: 15, fontStyle: 'bold', color: COLORS.brown.toColorString }" />
-      <Text :x="150" :y="240 + (i * 20)" :text="v.value.value" :style="{ fontSize: 15, fontStyle: 'bold', color: COLORS.brown.toColorString }" />
+      <Text :x="10" :y="270 + (i * 20)" :text="v.label" :style="{ fontSize: 15, fontStyle: 'bold', color: COLORS.brown.toColorString }" />
+      <Text :x="150" :y="270 + (i * 20)" :text="v.value.value" :style="{ fontSize: 15, fontStyle: 'bold', color: COLORS.brown.toColorString }" />
     </template>
   </MenuContainer>
 </template>
 
 <script>
 import { inject, computed } from 'vue'
-import { Text } from 'phavuer'
+import { Text, Image } from 'phavuer'
 import MenuContainer from '@/components/MenuContainer'
 import config from '@/data/config'
 import maps from '@/data/maps'
 const secToMinSecString = sec => `${Math.floor(sec / 60)}:${String(sec % 60).padStart(2, '0')}`
 export default {
-  components: { Text, MenuContainer },
+  components: { Text, Image, MenuContainer },
   setup () {
     const storage = inject('storage')
     const LIST = [
@@ -30,9 +31,11 @@ export default {
       { label: 'Charm of Body', value: computed(() => storage.state.status.body) },
       { label: 'Sum of Charm', value: computed(() => storage.state.status.heart + storage.state.status.body) }
     ]
+    const charmDiff = computed(() => Math.fix(storage.state.status.heart - storage.state.status.body, -6, 6))
     return {
       COLORS: config.COLORS,
-      LIST
+      LIST,
+      charmDiff
     }
   }
 }
