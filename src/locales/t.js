dd57edd3b18c386744c98be613e100ac68859a94
laineus <lainen000@gmail.com>
2020-12-29 11:48:12 +0900

    locales

--- src/locales/t.js
+++ src/locales/t.js
@@ -1,5 +1,18 @@
 import locales from '@/locales/index'
 import setting from '@/data/setting'
+
+const getReplaceText = values => text => {
+  if (['string', 'number'].includes(typeof values)) {
+    return text.replace(new RegExp('\\#\\{\\w+\\}', 'g'), values)
+  } else if (typeof values === 'object') {
+    return Object.keys(values).reduce((text, key) => {
+      return text.replace(new RegExp(`\\#\\{${key}\\}`, 'g'), values[key])
+    }, text)
+  } else {
+    return text
+  }
+}
+
 export default (key, values) => {
   const locale = locales[setting.state.lang]
   const text = key.split('.').reduce((obj, k) => obj && obj[k], locale)
@@ -7,12 +20,12 @@ export default (key, values) => {
     console.error(`Translation missing: "${key}" of "${setting.state.lang}"`)
     return 'Missing'
   }
-  if (!values) return text
-  if (['string', 'number'].includes(typeof values)) {
-    return text.replace(new RegExp('\\#\\{\\w+\\}', 'g'), values)
+  const replaceText = getReplaceText(values)
+  if (Array.isArray(text)) {
+    return text.map(replaceText)
+  } else if (typeof text === 'string') {
+    return replaceText(text)
   } else {
-    return Object.keys(values).reduce((text, key) => {
-      return text.replace(new RegExp(`\\#\\{${key}\\}`, 'g'), values[key])
-    }, text)
+    return text
   }
 }
