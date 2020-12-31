c674e9af00a10623a50d83948fe6b8a1afec77f3
laineus <lainen000@gmail.com>
2020-12-31 18:01:48 +0900

    forest2 objs

---
+++ src/map/forest2.js
@@ -0,0 +1,9 @@
+import { inject } from 'vue'
+export default {
+  name: '森2',
+  create () {
+    const field = inject('field')
+    const kajitsu = field.value.getObjectById(2)
+    const apple = field.value.getObjectById(4)
+  }
+}
