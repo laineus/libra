08b79b5dcff740dfc1df7e4e1c3d4f3578b2a306
laineus <lainen000@gmail.com>
2021-01-02 02:46:08 +0900

    camera

--- src/components/modules/setupCamera.js
+++ src/components/modules/setupCamera.js
@@ -6,19 +6,22 @@ export default (camera, width, height, player) => {
   camera.startFollow(player, true, 0.1, 0.1)
   // methods
   camera.updateFollow = () => camera.centerOn(camera._follow.x, camera._follow.y)
-  camera.look = (x, y, duration, relative = false) => {
-    const newX = relative ? (camera.scrollX + camera.centerX + x) : x
-    const newY = relative ? (camera.scrollY + camera.centerY + y) : y
-    const offsetX = newX - camera._follow.x
-    const offsetY = newY - camera._follow.y
+  camera.look = (x, y, duration) => {
+    const offsetX = x - camera._follow.x
+    const offsetY = y - camera._follow.y
     return new Promise(resolve => {
-      camera.pan(newX, newY, duration, 'Power2', false, (_, progress) => {
+      camera.pan(x, y, duration, 'Power2', false, (_, progress) => {
         if (progress === 1) {
           camera.setFollowOffset(-offsetX, -offsetY)
-          resolve()
+          resolve(() => camera.revert(duration))
         }
       })
     })
+  }
+  camera.move = (x, y, duration) => {
+    const absX = camera.scrollX + camera.centerX + x
+    const absY = camera.scrollY + camera.centerY + y
+    return camera.look(absX, absY, duration)
   }
   camera.revert = (duration = 0) => camera.look(camera._follow.x, camera._follow.y, duration)
   return camera
