94fdbe21290b61f0fc38742039afaaca36ce87b4
laineus <lainen000@gmail.com>
2021-01-09 02:51:26 +0900

    snake case

--- src/data/items.js
+++ src/data/items.js
@@ -1,15 +1,19 @@
+const snakeCase = str => {
+  str = str.charAt(0).toLowerCase() + str.slice(1)
+  return str.replace(/[A-Z]/g, s => `_${s.toLowerCase()}`)
+}
 const makeItem = (name, obj) => {
   return Object.assign({
     key: name,
     type: 'Substance',
-    texture: `item/${name}`
+    texture: `item/${snakeCase(name)}`
   }, obj)
 }
 const makeCharacter = (name, obj) => {
   return Object.assign({
     key: name,
     type: 'Character',
-    texture: `chara_sprite/${name}`
+    texture: `chara_sprite/${snakeCase(name)}`
   }, obj)
 }
 export default [
