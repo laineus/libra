ef998a9ed8b10f7bfa8f8278ab86b38664beeaa6
laineus <lainen000@gmail.com>
2021-01-09 01:31:07 +0900

    use tween prop

--- src/components/TapArea.vue
+++ src/components/TapArea.vue
@@ -1,12 +1,11 @@
 <template>
   <Container ref="tapArea" :depth="100000" @pointerdown="onTap" @preUpdate="update">
-    <Image texture="speach_bubbles" :y="-20" :frame="frame" @create="initBubbleAnim" />
+    <Image texture="speach_bubbles" :y="-20" :frame="frame" :tween="{ y: -24, yoyo: true, repeat: -1, duration: 500 }" />
   </Container>
 </template>
 
 <script>
 import { refObj, Container, Image } from 'phavuer'
-import { inject } from 'vue'
 export default {
   components: { Container, Image },
   props: {
@@ -15,18 +14,14 @@ export default {
   },
   emits: ['tap'],
   setup (props, context) {
-    const scene = inject('scene')
     const tapArea = refObj(null)
     const onTap = () => context.emit('tap')
-    const initBubbleAnim = obj => {
-      scene.add.tween({ targets: obj, y: -24, yoyo: true, repeat: -1, duration: 500 })
-    }
     const update = () => {
       if (props.follow) tapArea.value.setPosition(props.follow.x, props.follow.y - tapArea.value.height.half + 10)
     }
     return {
       tapArea,
-      onTap, update, initBubbleAnim
+      onTap, update
     }
   }
 }
