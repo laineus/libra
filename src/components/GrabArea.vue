50b79b3082326a98585f7969716062eab5271fcb
laineus <lainen000@gmail.com>
2021-01-09 01:25:13 +0900

    grab icon

--- src/components/GrabArea.vue
+++ src/components/GrabArea.vue
@@ -1,12 +1,12 @@
 <template>
   <Container :depth="100000" :x="data.x" :y="data.y" @pointerdown="grab" @preUpdate="update">
-    <Image texture="hand_catch" :x="10" :alpha="data.alpha" :tint="data.tint" @create="createHand" />
+    <Image texture="hand_catch" :x="10" :alpha="data.grabbing ? 1 : 0.5" :tint="data.grabbing ? 0xFF0000 : config.COLORS.white" :tween="data.grabbing ? null : handTween" />
   </Container>
 </template>
 
 <script>
 import { Container, Image } from 'phavuer'
-import { inject, reactive } from 'vue'
+import { inject, reactive, shallowReactive } from 'vue'
 import config from '@/data/config'
 export default {
   components: { Container, Image },
@@ -17,18 +17,16 @@ export default {
   emits: ['grab', 'capture', 'cancel'],
   setup (props, context) {
     const menu = inject('menu')
-    const scene = inject('scene')
-    const data = reactive({
-      x: 0, y: 0, tint: config.COLORS.white, alpha: 0.5
+    const data = shallowReactive({
+      x: 0, y: 0, grabbing: false
     })
+    const handTween = reactive({ x: 13, y: 3, yoyo: true, repeat: -1, duration: 700 })
     const grab = () => {
       menu.value.select('bag').then(menuBag => {
-        data.tint = 0xFF0000
-        data.alpha = 1
+        data.grabbing = true
         context.emit('grab')
         menuBag.grabItem({ key: props.name }, 'capture').then(bool => {
-          data.tint = config.COLORS.white
-          data.alpha = 0.5
+          data.grabbing = false
           context.emit(bool ? 'capture' : 'cancel')
         })
       })
@@ -39,12 +37,10 @@ export default {
         data.y = props.follow.y
       }
     }
-    const createHand = obj => {
-      scene.add.tween({ targets: obj, x: 13, y: 3, yoyo: true, repeat: -1, duration: 700 })
-    }
     return {
+      config,
       data,
-      createHand,
+      handTween,
       grab, update
     }
   }
