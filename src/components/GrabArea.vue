cefa8e1baf7d51869c43d963f14e34e1193e3863
laineus <lainen000@gmail.com>
2021-01-09 00:21:25 +0900

    hand

--- src/components/GrabArea.vue
+++ src/components/GrabArea.vue
@@ -1,12 +1,15 @@
 <template>
-  <Container :depth="100000" :x="data.x" :y="data.y" @pointerdown="grab" @preUpdate="update" />
+  <Container :depth="100000" :x="data.x" :y="data.y" @pointerdown="grab" @preUpdate="update">
+    <Image texture="hand_catch" :x="10" :alpha="data.alpha" :tint="data.tint" @create="createHand" />
+  </Container>
 </template>
 
 <script>
-import { Container } from 'phavuer'
+import { Container, Image } from 'phavuer'
 import { inject, reactive } from 'vue'
+import config from '@/data/config'
 export default {
-  components: { Container },
+  components: { Container, Image },
   props: {
     follow: { default: null },
     name: { default: null }
@@ -14,13 +17,18 @@ export default {
   emits: ['grab', 'capture', 'cancel'],
   setup (props, context) {
     const menu = inject('menu')
+    const scene = inject('scene')
     const data = reactive({
-      x: 0, y: 0
+      x: 0, y: 0, tint: config.COLORS.white, alpha: 0.5
     })
     const grab = () => {
       menu.value.select('bag').then(menuBag => {
+        data.tint = 0xFF0000
+        data.alpha = 1
         context.emit('grab')
         menuBag.grabItem({ key: props.name }, 'capture').then(bool => {
+          data.tint = config.COLORS.white
+          data.alpha = 0.5
           context.emit(bool ? 'capture' : 'cancel')
         })
       })
@@ -31,8 +39,12 @@ export default {
         data.y = props.follow.y
       }
     }
+    const createHand = obj => {
+      scene.add.tween({ targets: obj, x: 13, y: 3, yoyo: true, repeat: -1, duration: 700 })
+    }
     return {
       data,
+      createHand,
       grab, update
     }
   }
