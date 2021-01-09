3527cfa0300120b43f3106bafdfdfa20f82a66fe
laineus <lainen000@gmail.com>
2021-01-09 13:28:34 +0900

    revert debug

--- src/components/MenuSystem.vue
+++ src/components/MenuSystem.vue
@@ -1,7 +1,7 @@
 <template>
   <MenuContainer ref="container" :arrowX="25 + (3 * 60)" :height="300" :title="'System'">
     <Container v-for="(v, i) in tabs" :key="i" :x="6.5 + (i * 67)" :y="6.5">
-      <RoundRectangle @create="rr" :width="60" :height="24" :lineWidth="1" :strokeColor="COLORS.brown" :radius="6" :fillColor="index === i ? COLORS.brown : null" @pointerdown="index = i" />
+      <RoundRectangle :width="60" :height="24" :lineWidth="1" :strokeColor="COLORS.brown" :radius="6" :fillColor="index === i ? COLORS.brown : null" @pointerdown="index = i" />
       <Text :text="v.label" :origin="0.5" :x="30" :y="12" :size="13" :bold="true" :color="index === i ? 'soy' : 'brown'" />
     </Container>
     <MenuSystemSave v-if="index === 0" :offsetX="offsetX" :offsetY="offsetY + 32" :y="32" />
@@ -31,11 +31,7 @@ export default {
       container,
       tabs, index,
       offsetX: computed(() => container.value?.offsetX),
-      offsetY: computed(() => container.value?.offsetY),
-      rr: obj => {
-        console.log(obj, obj.setPipeline)
-        // obj.texture.setFilter
-      }
+      offsetY: computed(() => container.value?.offsetY)
     }
   }
 }
