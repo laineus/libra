1400b9eb63f5da180204f87eb1916294f712010f
laineus <lainen000@gmail.com>
2021-01-09 22:53:10 +0900

    event

--- src/map/forest3.js
+++ src/map/forest3.js
@@ -17,10 +17,12 @@ export default {
     const bag = inject('bag')
 
     const torrent = field.getObjectById(3)
-    // const flog = field.getObjectById(4)
-
-    const tTorrent = new Talker('NPC', torrent.object)
+    const tTorrent = new Talker('トレント', torrent.object)
     const speakTorrent = talk.getSpeakScripts(tTorrent)
+    const flog = field.getObjectById(4)
+    const tFlog = new Talker('カエル', flog.object)
+    const speakFlog = talk.getSpeakScripts(tFlog)
+
     const torrentEvent1 = async () => {
       const scripts = t('events.torrentFlog.torrent1')
       await speakTorrent(scripts.splice(0, 2))
@@ -38,10 +40,30 @@ export default {
         await speakTorrent(t('events.torrentFlog.torrent2.started'))
       }
     }
+    const torrentEvent3 = async () => {
+      await speakTorrent('aaa')
+      state.events.torrentFlog = STEPS.COMPLETED
+    }
     torrent.setTapEvent(computed(() => {
       if (state.events.torrentFlog === STEPS.NULL) return torrentEvent1
-      if (state.events.torrentFlog === STEPS.STARTED) return torrentEvent2
-      return null
+      if (state.events.torrentFlog <= STEPS.FOUND) return torrentEvent2
+      if (state.events.torrentFlog === STEPS.GAVE) return torrentEvent3
+      return async () => speakTorrent('end')
     }))
+    flog.setTapEvent(async () => {
+      if (state.events.torrentFlog === STEPS.FOUND && bag.hasItem('hercules')) {
+        const scripts = t('events.torrentFlog.flog.give')
+        await speakFlog(scripts.splice(0, 4))
+        const accepted = await uiScene.setSelector(scripts.shift()) === 0
+        accepted ? scripts.shift() : await speakFlog(scripts.shift())
+        uiScene.log.push(scripts.shift()[accepted ? 0 : 1])
+        await speakFlog(scripts.splice(0, 3))
+        await uiScene.setSelector(scripts.shift())
+        await speakFlog(scripts)
+        state.events.torrentFlog = STEPS.GAVE
+      } else {
+        await speakFlog(t('events.torrentFlog.flog.greeting'))
+      }
+    })
   }
 }
