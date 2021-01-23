774502cbeb01a04b4728dfb43f4760f05d256caa
laineus <lainen000@gmail.com>
2021-01-23 18:08:33 +0900

    snake

--- src/data/items.js
+++ src/data/items.js
@@ -48,6 +48,7 @@ export default [
   makeCharacter('flog'),
   makeCharacter('hercules'),
   makeCharacter('bat'),
+  makeCharacter('snake'),
   makeCharacter('penguin'),
   makeCharacter('amili')
 ]
