d8e16357f00030192de137d928d78ed350f9a22c
lainues <lainen000@gmail.com>
2021-02-06 11:05:25 +0900

    leave and chase

--- src/components/modules/useFollowing.js
+++ src/components/modules/useFollowing.js
@@ -8,7 +8,11 @@ export default chara => {
   })
   let randomWalk = null
   const targetObject = ref(null)
-  const setTargetObject = object => targetObject.value = object
+  const leaveFromTargetObject = ref(false)
+  const setTargetObject = (object, leave = false) => {
+    targetObject.value = object
+    leaveFromTargetObject.value = leave
+  }
   let resolver = null
   const setTargetPosition = (x, y) => {
     targetPosition.x = x
@@ -37,11 +41,14 @@ export default chara => {
     const diffX = getDiffToTargetPositionX()
     const diffY = getDiffToTargetPositionY()
     const distance = getDiffToTargetPositionDistance(diffX, diffY)
-    if (targetObject.value && distance < 50) return clearTargetPosition()
+    if (targetObject.value) {
+      if (!leaveFromTargetObject.value && (distance < 50 || distance > 200)) return clearTargetPosition()
+      if (leaveFromTargetObject.value && distance > 160) return clearTargetPosition()
+    }
     const body = unref(chara).body
     const x = (!body.blocked.left && !body.blocked.right) ? diffX : diffX * 0.1
     const y = (!body.blocked.top && !body.blocked.down) ? diffY : diffY * 0.1
-    body.setVelocity(x, y)
+    leaveFromTargetObject.value ? body.setVelocity(-x, -y) : body.setVelocity(x, y)
     const spd = Math.min(speed, distance * 10)
     body.velocity.normalize().scale(spd)
     if (distance < 5) clearTargetPosition()
