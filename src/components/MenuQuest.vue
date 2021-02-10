51dc2b6a954c3c5a6bd25c8b68fbebc51f1045b9
lainues <lainen000@gmail.com>
2021-02-11 03:12:36 +0900

    quest checked

--- src/components/MenuQuest.vue
+++ src/components/MenuQuest.vue
@@ -1,27 +1,35 @@
 <template>
   <MenuContainer ref="container" :arrowX="25 + (1 * 60)" :height="305" title="Quest" @wheel="onWheel" @pointermove="onSwipe">
-    <Container v-for="(v, i) in quest.slice(offset, offset + 8)" :key="i" :x="rowWidth.half" :y="(i * rowHeight) + rowHeight.half" :width="rowWidth" :height="rowHeight" @pointerup="p => tapItem(p, i)">
+    <Container v-for="(v, i) in quest.slice(offset, offset + 8)" :key="i" :visible="!selected" :x="rowWidth.half" :y="(i * rowHeight) + rowHeight.half" :width="rowWidth" :height="rowHeight" @pointerup="p => tapItem(p, i)">
       <Line v-if="i !== 8 - 1" :x="0" :y="rowHeight.half" :lineWidth="0.5" :x2="rowWidth" :strokeColor="COLORS.brown" :alpha="0.25" />
       <Text :x="-rowWidth.half + 10" :y="0" :originY="0.5" :text="t(`quest.${v.key}.title`)" :size="13" :bold="true" />
+      <Image :x="rowWidth.half - 12" :y="0" :originY="0.5" texture="check" frame="1" :tint="COLORS.brown" v-if="v.completed(state)" />
     </Container>
-    <RoundRectangle :x="rowWidth + 13 - 3" :y="3" :width="5" :height="289" :radius="3" :fillColor="COLORS.brown" :originX="1" :originY="0" :alpha="0.3" />
-    <RoundRectangle :x="rowWidth + 13 - 3" :y="3 + (289 / quest.length * offset)" :width="5" :height="289 * (8 / quest.length)" :radius="3" :fillColor="COLORS.brown" :originX="1" :originY="0" />
+    <Container v-if="selected">
+      <Text :x="10" :y="10" :text="t(`quest.${selected.key}.title`)" :size="13" :bold="true" />
+    </Container>
+    <Container :visible="!selected" :x="rowWidth + 13 - 3" :y="3">
+      <RoundRectangle :width="5" :height="289" :radius="3" :fillColor="COLORS.brown" :originX="1" :originY="0" :alpha="0.3" />
+      <RoundRectangle :y="289 / quest.length * offset" :width="5" :height="289 * (8 / quest.length)" :radius="3" :fillColor="COLORS.brown" :originX="1" :originY="0" />
+    </Container>
   </MenuContainer>
 </template>
 
 <script>
-import { reactive, ref, toRefs } from 'vue'
-import { Container, RoundRectangle, Line } from 'phavuer'
+import { inject, reactive, ref, toRefs } from 'vue'
+import { Container, RoundRectangle, Line, Image } from 'phavuer'
 import MenuContainer from '@/components/MenuContainer'
 import Text from '@/components/Text'
 import quest from '@/data/quest'
 import config from '@/data/config'
 export default {
-  components: { MenuContainer, Container, RoundRectangle, Text, Line },
+  components: { MenuContainer, Container, RoundRectangle, Text, Line, Image },
   emits: ['close'],
   setup (_, context) {
+    const state = inject('storage').state
     const container = ref(null)
     const data = reactive({
+      selected: null,
       offset: 0,
       rowWidth: 207, rowHeight: 37,
       selectedIndex: null
@@ -39,9 +47,11 @@ export default {
       scrollY = 0
     }
     const tapItem = (pointer, i) => {
+      data.selected = quest[i]
     }
     return {
       t,
+      state,
       COLORS: config.COLORS,
       quest,
       container,
