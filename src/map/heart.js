44503c688f23e3dcf646380718cb1fda8296afac
lainues <lainen000@gmail.com>
2021-02-13 01:42:25 +0900

    heart

--- src/map/heart.js
+++ src/map/heart.js
@@ -1,4 +1,4 @@
-import { computed, inject } from 'vue'
+import { inject } from 'vue'
 import Talker from '@/util/Talker'
 import { MAIN_STEPS } from '@/data/eventSteps'
 export default {
@@ -8,36 +8,64 @@ export default {
     const talk = inject('talk').value
     const state = inject('storage').state
     const libra = inject('player').value
+    const event = inject('event')
 
-    const area = field.getObjectById(2)
     const kajitsu = field.getObjectById(3)
     const tLibra = new Talker(t('name.libra'), libra.object)
     const tKajitsu = new Talker(t('name.kajitsu'), kajitsu.object)
     const speakLibra = talk.getSpeakScripts(tLibra)
     const speakKajitsu = talk.getSpeakScripts(tKajitsu)
 
-    kajitsu.setVisible(computed(() => state.events.main < MAIN_STEPS.HEART))
+    kajitsu.setVisible(false)
 
-    const event = async () => {
-      await libra.setTargetPosition((15).toPixelCenter, (18).toPixel)
-      kajitsu.lookTo('left')
-      await libra.setTargetPosition((15).toPixelCenter, (16).toPixel)
-      await sleep(1000)
-      await speakKajitsu(t('events.heart.start1'))
-      await sleep(500)
-      libra.lookTo('right')
-      await sleep(500)
-      await speakKajitsu(t('events.heart.start2'))
-      await speakLibra(t('events.libra.exclamation'))
-      await sleep(500)
-      await speakKajitsu(t('events.heart.start3'))
-      await speakLibra(t('events.libra.silence'))
-      await speakKajitsu(t('events.heart.start4'))
-      const completeTransition = await uiScene.transition(1000)
-      libra.lookTo('up')
-      state.events.main = MAIN_STEPS.HEART
-      await completeTransition()
+    if (state.events.main === MAIN_STEPS.CROSSING) {
+      kajitsu.setVisible(true)
+      event.exec(async () => {
+        await sleep(500)
+        await libra.setTargetPosition(field.positions.relay.x, field.positions.relay.y)
+        kajitsu.lookTo('left')
+        await libra.setTargetPosition(field.positions.center.x, field.positions.center.y)
+        await sleep(1000)
+        await speakKajitsu(t('events.heart.start1'))
+        await sleep(500)
+        libra.lookTo('right')
+        await sleep(500)
+        await speakKajitsu(t('events.heart.start2'))
+        await speakLibra(t('events.libra.exclamation'))
+        await sleep(500)
+        await speakKajitsu(t('events.heart.start3'))
+        await speakLibra(t('events.libra.silence'))
+        await speakKajitsu(t('events.heart.start4'))
+        const completeTransition = await uiScene.transition(1000)
+        libra.lookTo('up')
+        kajitsu.setVisible(false)
+        state.events.main = MAIN_STEPS.HEART
+        await completeTransition()
+      })
     }
-    area.setEvent(computed(() => state.events.main < MAIN_STEPS.HEART ? event : null))
+    if (state.events.main === MAIN_STEPS.BRAIN) {
+      kajitsu.setVisible(true)
+      event.exec(async () => {
+        await sleep(500)
+        await libra.setTargetPosition(field.positions.relay.x, field.positions.relay.y)
+        kajitsu.lookTo('left')
+        await libra.setTargetPosition(field.positions.center.x, field.positions.center.y)
+        await sleep(1000)
+        await speakKajitsu(t('events.dream.kajitsu1'))
+        libra.lookTo('right')
+        await sleep(500)
+        await speakKajitsu(t('events.dream.kajitsu2'))
+        await speakLibra(t('events.libra.silence'))
+        await speakKajitsu(t('events.dream.kajitsu3'))
+        const cancel = await uiScene.setSelector(t('events.dream.yesno')) === 1
+        if (cancel) {
+          await speakKajitsu(t('events.dream.cancel'))
+          const completeTransition = await uiScene.transition(1000)
+          kajitsu.setVisible(false)
+          await completeTransition()
+          return
+        }
+      })
+    }
   }
 }
