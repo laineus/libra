15f0ed5a86c93b92cc850e9d63de09b840ba2cb0
lainues <lainen000@gmail.com>
2021-02-14 03:22:41 +0900

    load

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -7,10 +7,15 @@
     <Image texture="nav" :frame="i" @pointerdown.stop="tapItem(i)" />
     <Text :text="v" :origin="0.5" />
   </Container>
-  <Container v-if="selected" :x="config.WIDTH.half" :y="400">
+  <Container v-if="selected === 1" :x="config.WIDTH.half" :y="375">
+    <RoundRectangle :width="266" :height="276" :origin="0.5" :radius="5" :fillColor="config.COLORS.soy" @pointerdown.stop />
+    <RoundRectangle :width="261" :height="271" :origin="0.5" :radius="5" :strokeColor="config.COLORS.brown" :lineWidth="1" />
+    <menu-system-save v-if="selected === 1" :x="-110" :y="-130" />
+  </Container>
+  <Container v-else-if="selected === 2" :x="config.WIDTH.half" :y="400">
     <RoundRectangle :width="266" :height="206" :origin="0.5" :radius="5" :fillColor="config.COLORS.soy" @pointerdown.stop />
     <RoundRectangle :width="261" :height="201" :origin="0.5" :radius="5" :strokeColor="config.COLORS.brown" :lineWidth="1" />
-    <menu-system-config v-if="selected === 2" :x="-110" :y="-94" />
+    <menu-system-config :x="-110" :y="-94" />
     <RoundRectangle :width="101" :height="23" :origin="0.5" :radius="5" :strokeColor="config.COLORS.brown" :lineWidth="1" :y="72" @pointerdown="selected = null" />
     <Text text="OK" :origin="0.5" :y="72" :size="14" />
   </Container>
@@ -21,9 +26,10 @@ import { inject, ref } from 'vue'
 import { Rectangle, Image, Container, RoundRectangle } from 'phavuer'
 import config from '@/data/config'
 import Text from '@/components/Text'
+import MenuSystemSave from '@/components/MenuSystemSave'
 import MenuSystemConfig from '@/components/MenuSystemConfig'
 export default {
-  components: { Rectangle, Text, Image, Container, RoundRectangle, MenuSystemConfig },
+  components: { Rectangle, Text, Image, Container, RoundRectangle, MenuSystemSave, MenuSystemConfig },
   emits: ['close'],
   setup (_, context) {
     const gameScene = inject('gameScene')
