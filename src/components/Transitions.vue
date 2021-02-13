3594e6decb706c57b0ca2cdf6402aef77eedede9
lainues <lainen000@gmail.com>
2021-02-13 10:32:41 +0900

    make component

---
+++ src/components/Transitions.vue
@@ -0,0 +1,40 @@
+<template>
+  <Rectangle v-for="v in transitions" :key="v.id" :fillColor="v.color" :origin="0" :width="config.WIDTH" :height="config.HEIGHT" :depth="config.DEPTH.TRANSITION" :alpha="v.alpha" />
+</template>
+
+<script>
+import { inject, shallowReactive } from 'vue'
+import { Rectangle } from 'phavuer'
+import config from '@/data/config'
+export default {
+  components: { Rectangle },
+  setup () {
+    const scene = inject('scene')
+    const transitions = shallowReactive([])
+    const getTransitionTween = target => (duration, { alpha, hold, destroy }) => {
+      return new Promise(resolve => {
+        const onComplete = () => {
+          if (destroy) transitions.delete(target)
+          sleep(hold).then(resolve)
+        }
+        return scene.add.tween({ targets: target, duration, alpha, onComplete })
+      })
+    }
+    const add = (duration = 500, { color = config.COLORS.black, alpha = 1, hold } = {}) => {
+      hold = hold ?? duration.half
+      const data = shallowReactive({ id: Symbol('id'), color, alpha: 0 })
+      transitions.push(data)
+      const tween = getTransitionTween(data)
+      return tween(duration, { alpha, hold, destroy: false }).then(() => {
+        const nextTween = (...args) => tween(args[0] ?? duration, Object.assign({ alpha: 0, hold: 0, destroy: true }, args[1])).then(() => nextTween)
+        return nextTween
+      })
+    }
+    return {
+      config,
+      transitions,
+      add
+    }
+  }
+}
+</script>
--- src/components/UIScene.vue
+++ src/components/UIScene.vue
@@ -15,14 +15,14 @@
         <Text :tween="{ alpha: 1, duration: 300, yoyo: true, hold: 3000, onComplete: () => setMapName(null) }" :alpha="0" :text="mapName" :origin="0.5" color="soy" :bold="true" />
       </Container>
     </template>
-    <Rectangle v-for="v in transitions" :key="v.id" :fillColor="v.color" :origin="0" :width="config.WIDTH" :height="config.HEIGHT" :depth="config.DEPTH.TRANSITION" :alpha="v.alpha" />
+    <Transitions ref="transitions" />
     <Text v-if="screenMessage" :text="screenMessage" :x="config.WIDTH.half" :y="config.HEIGHT.half" :size="17" color="white" :origin="0.5" :depth="config.DEPTH.TRANSITION" />
   </Scene>
 </template>
 
 <script>
-import { inject, onMounted, reactive, ref, shallowReactive } from 'vue'
-import { refScene, Scene, Rectangle, Circle, Image, Container, RoundRectangle } from 'phavuer'
+import { inject, onMounted, reactive, ref } from 'vue'
+import { refScene, Scene, Circle, Image, Container, RoundRectangle } from 'phavuer'
 import Title from './Title'
 import Controller from './Controller'
 import Talk from './Talk'
@@ -30,9 +30,10 @@ import Selector from './Selector'
 import Menu from './Menu'
 import Log from './Log'
 import Text from './Text'
+import Transitions from './Transitions'
 import config from '@/data/config'
 export default {
-  components: { Scene, Title, Controller, Rectangle, Circle, Image, Container, RoundRectangle, Talk, Selector, Menu, Log, Text },
+  components: { Scene, Title, Controller, Circle, Image, Container, RoundRectangle, Talk, Selector, Menu, Log, Text, Transitions },
   setup (props) {
     const mobile = inject('mobile')
     const frames = inject('frames')
@@ -45,7 +46,8 @@ export default {
       controller: ref(null),
       talk: ref(null),
       log: ref(null),
-      menu: ref(null)
+      menu: ref(null),
+      transitions: ref(null)
     }
     onMounted(() => {
       refs.scene.value.input.setTopOnly(false)
@@ -72,26 +74,6 @@ export default {
       if (!field.value) return
       nealestCheckable.value = field.value.charas.concat(field.value.substances).map(v => v.ref.value).filter(v => v.checkable).findMin(v => v.distanceToPlayer)
     }
-    const transitions = shallowReactive([])
-    const getTransitionTween = target => (duration, { alpha, hold, destroy }) => {
-      return new Promise(resolve => {
-        const onComplete = () => {
-          if (destroy) transitions.delete(target)
-          sleep(hold).then(resolve)
-        }
-        return refs.scene.value.add.tween({ targets: target, duration, alpha, onComplete })
-      })
-    }
-    const transition = (duration = 500, { color = config.COLORS.black, alpha = 1, hold } = {}) => {
-      hold = hold ?? duration.half
-      const data = shallowReactive({ id: Symbol('id'), color, alpha: 0 })
-      transitions.push(data)
-      const tween = getTransitionTween(data)
-      return tween(duration, { alpha, hold, destroy: false }).then(() => {
-        const nextTween = (...args) => tween(args[0] ?? duration, Object.assign({ alpha: 0, hold: 0, destroy: true }, args[1])).then(() => nextTween)
-        return nextTween
-      })
-    }
     const mapName = ref(null)
     const setMapName = name => {
       mapName.value = name
@@ -103,12 +85,11 @@ export default {
       update,
       ...refs,
       titleScreen,
-      transition,
-      transitions,
       nealestCheckable,
       selector, setSelector,
       screenMessage, setScreenMessage,
       mapName, setMapName,
+      transition: (...args) => refs.transitions.value.add(...args),
       check: () => {
         if (!nealestCheckable.value) return
         nealestCheckable.value.execTapEvent()
