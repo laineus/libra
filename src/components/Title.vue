4f01df77c1ec93417476d1a649533fe4607ff34a
lainues <lainen000@gmail.com>
2021-02-14 02:18:24 +0900

    title

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,30 +1,35 @@
 <template>
-  <Rectangle :fillColor="0x333333" :origin="0" :width="config.WIDTH" :height="config.HEIGHT" @pointerdown="tap" />
+  <Rectangle :fillColor="0x333333" :origin="0" :width="config.WIDTH" :height="config.HEIGHT" />
+  <Image texture="main" :x="config.WIDTH.half" :y="config.HEIGHT.half" />
+  <Image texture="logo" :x="config.WIDTH.half" :y="config.HEIGHT.half - 80" />
   <Text :text="`${v.name} ${v.state?.map ?? 'empty'}`" color="white" v-for="(v, i) in list" :key="v.number" :x="40" :y="40 + (i * 30)" @pointerdown="load(v)" />
+  <Container v-for="(v, i) in list" :key="i" :x="config.WIDTH.half" :y="380 + (i * 40)">
+    <Image texture="nav" :frame="i" @pointerdown="tapItem(i)" />
+    <Text :text="v" :origin="0.5" />
+  </Container>
 </template>
 
 <script>
-import { inject, ref } from 'vue'
-import { Rectangle } from 'phavuer'
+import { inject } from 'vue'
+import { Rectangle, Image, Container } from 'phavuer'
 import config from '@/data/config'
 import Text from '@/components/Text'
 export default {
-  components: { Rectangle, Text },
+  components: { Rectangle, Text, Image, Container },
   emits: ['close'],
   setup (_, context) {
     const gameScene = inject('gameScene')
     const storage = inject('storage')
-    const list = ref(null)
-    storage.getList().then(v => {
-      list.value = v
-    })
+    const list = ['はじめから', 'つづきから', '設定']
     return {
       config,
       list,
-      tap: async () => {
-        // await gameScene.value.setField('forest1', 300, 552, -Math.PI.half)
-        await gameScene.value.setField('forest3', 800, 452, -Math.PI.half)
-        context.emit('close')
+      tapItem: async i => {
+        if (i === 0) {
+          // await gameScene.value.setField('forest1', 300, 552, -Math.PI.half)
+          await gameScene.value.setField('forest3', 800, 452, -Math.PI.half)
+          context.emit('close')
+        }
       },
       load: async data => {
         if (!data.state) return
