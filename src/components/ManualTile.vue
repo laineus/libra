72b34e7793557b2ee66dbf729aa91163cff8e3fb
lainues <lainen000@gmail.com>
2021-02-23 17:35:07 +0900

    depth

--- src/components/ManualTile.vue
+++ src/components/ManualTile.vue
@@ -1,5 +1,5 @@
 <template>
-  <Image ref="object" :texture="texture" :x="setting.x" :y="setting.y" :displayWidth="setting.width" :displayHeight="setting.height" :originX="0" :originY="1">
+  <Image ref="object" :texture="texture" :x="setting.x" :y="setting.y" :displayWidth="setting.width" :displayHeight="setting.height" :originX="0" :originY="1" :depth="depth">
     <StaticBody v-if="physics && object" :width="physics.width * object.scaleX" :height="physics.height * object.scaleY" :offsetX="physics.x * object.scaleX" :offsetY="physics.y * object.scaleY" />
   </Image>
 </template>
@@ -7,22 +7,29 @@
 <script>
 import { Image, refObj, StaticBody } from 'phavuer'
 import { inject } from 'vue'
+import config from '@/data/config'
 export default {
   components: { Image, StaticBody },
   props: ['setting', 'field'],
   setup (props) {
     const scene = inject('scene')
     const object = refObj(null)
-    const tileset = scene.cache.tilemap.get(props.field.name).data.tilesets.filter(v => v.tiles).find(v => {
+    const map = scene.cache.tilemap.get(props.field.name).data
+    const originalLayerIndex = map.layers.findIndex(v => v.objects?.some(o => o.id === props.setting.id))
+    const layerIndex = map.layers.slice(0, originalLayerIndex).count(v => v.type === 'tilelayer') - 1
+    const tileset = map.tilesets.filter(v => v.tiles).find(v => {
       const maxId = v.tiles.findMax(t => t.id).id
       return props.setting.gid >= v.firstgid && props.setting.gid < (v.firstgid + maxId + 1)
     })
     const tile = tileset.tiles.find(v => (v.id + tileset.firstgid) === props.setting.gid)
+    const depthName = tile.properties?.find(v => v.name === 'depth')?.value
+    const depth = depthName === 'SUBSTANCE' ? props.setting.y : (config.DEPTH[depthName] ?? layerIndex)
     const texture = `tileset/${tile.image.split('/').slice(-1)[0].split('.')[0]}`
     const physics = tile.objectgroup?.objects[0]
     return {
       object, physics,
-      texture
+      texture,
+      depth
     }
   }
 }
