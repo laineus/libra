28a3ae17d66420bd6ad325d6bb2e2434e49c854c
lainues <lainen000@gmail.com>
2021-03-12 18:02:34 +0900

    menu icons

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,7 +2,7 @@
   <div>
     <Container v-for="(v, i) in menu" :key="i" :x="(235).byRight + (i * 50)" :y="(35).byBottom">
       <Circle :fillColor="COLORS.brown" :radius="20" :lineWidth="2" :strokeColor="COLORS.soy" @pointerdown="(...args) => tapButton(i, ...args)" />
-      <Image texture="menu_icons" :frame="i" :tint="COLORS.soy" :scale="0.8" />
+      <Image texture="menu_icons" :frame="i" :tint="COLORS.soy" />
     </Container>
     <template v-if="selected">
       <Container :depth="-1" :x="config.WIDTH.half" :y="config.HEIGHT.half" :width="config.WIDTH" :height="config.HEIGHT" @pointerdown="tapCloseArea" />
