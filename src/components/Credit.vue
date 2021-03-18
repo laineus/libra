9ddf7a2e5818ed1f518f907b53f6743e8c5814f5
Laineus <lainen000@gmail.com>
2021-03-19 00:17:42 +0900

    credit

--- src/components/Credit.vue
+++ src/components/Credit.vue
@@ -1,27 +1,42 @@
 <template>
-  <Container>
-    <Rectangle :width="config.WIDTH" :height="config.HEIGHT" :fillColor="config.COLORS.black" :origin="0" />
-    <Container v-for="(v, i) in list" :key="i" :x="config.WIDTH.half" :y="list.slice(0, i).sum(v => v.names.length * 30) + (i * 120)">
-      <Text :text="`- ${v.title} -`" :origin="0.5" color="white" />
-      <Text v-for="(name, i2) in v.names" :key="i2" :text="name" :size="19" :origin="0.5" color="white" :y="30 + i2 * 30" />
+  <Container :y="pos.y">
+    <Rectangle :width="config.WIDTH" :height="5000" :fillColor="config.COLORS.black" :origin="0" @wheel="p => pos.y -= p.deltaY" />
+    <Container :x="config.WIDTH.half">
+      <Container v-for="(v, i) in list" :key="i" :y="list.slice(0, i).sum(v => v.names.length * 40) + (i * 180) + config.HEIGHT">
+        <Text :text="`- ${v.title} -`" :originX="0.5" color="white" />
+        <Text v-for="(name, i2) in v.names" :key="i2" :text="name" :size="19" :originX="0.5" color="white" :y="40 + i2 * 40" />
+      </Container>
+      <!-- Site -->
+      <Text text="- Official Web site -" :originX="0.5" color="white" :y="1700" />
+      <Image texture="banner_libra" :y="1760" />
+      <Text text="https://dream-libra.laineus.com" :originX="0.5" color="white" :y="1800" />
+      <!-- Site -->
+      <Image texture="phaser3" :y="2200" />
+      <Image texture="tiled" :y="2500" />
+      <Image texture="phavuer" :y="2800" />
+      <Image texture="drive_l" :y="3600" />
     </Container>
   </Container>
+  <div>{{ Math.round(pos.y) }}</div>
 </template>
 
 <script>
-import { Container, Rectangle } from 'phavuer'
+import { Container, Rectangle, Image } from 'phavuer'
 import Text from '@/components/Text'
 import config from '@/data/config'
+import { reactive } from '@vue/reactivity'
 export default {
-  components: { Container, Rectangle, Text },
+  components: { Container, Rectangle, Image, Text },
   setup () {
     return {
+      pos: reactive({ y: 0 }),
       config,
       list: [
         { title: 'Written by', names: ['Laineus'] },
         { title: 'Graphics by', names: ['Laineus'] },
         { title: 'Music by', names: ['Laineus', 'Miku Hatsune'] },
-        { title: 'Developed by', names: ['Laineus'] }
+        { title: 'Developed by', names: ['Laineus'] },
+        { title: 'Theme song', names: ['『ハッピーエンド』'] }
       ]
     }
   }
