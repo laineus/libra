4b0459d89ddb0bb8689e90d65f3413f93fa4f8a0
lainues <lainen000@gmail.com>
2021-03-19 20:55:25 +0900

    frame

--- src/components/modules/useFrameAnim.js
+++ src/components/modules/useFrameAnim.js
@@ -1,23 +1,29 @@
+import { ref } from 'vue'
+const DEFAULT = Symbol('default')
 export default settings => {
+  if (!Array.isArray(settings)) settings = [settings]
   const patterns = {}
   let tick = 0
   let lastPlayedKey = null
-  settings.forEach(({ key, frames, duration }) => {
+  const frame = ref(settings[0].frames[0])
+  settings.forEach(({ key = DEFAULT, frames, duration }) => {
     patterns[key] = tick => {
       const i = Math.floor(tick / duration) % frames.length
       return frames[i]
     }
   })
-  const play = key => {
+  const play = (key = DEFAULT) => {
     if (key !== lastPlayedKey) {
       tick = 0
       lastPlayedKey = key
     } else {
       tick++
     }
-    return patterns[key](tick)
+    frame.value = patterns[key](tick)
+    return frame.value
   }
   return {
-    play
+    play,
+    frame
   }
 }
