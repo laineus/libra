fe3bb765268b42fddcb2b42b5019aca418e2fc18
Laineus <lainen000@gmail.com>
2021-04-11 09:32:05 +0900

    select item

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -4,7 +4,7 @@
       <Image texture="select_bg" :origin="0.5" :x="bgWidth.half" :y="sumHeight.half" :scaleX="(bgWidth + 40) / 162" :scaleY="(sumHeight + 30) / 84" />
       <Image texture="select_arrow" :origin="0.5" :x="bgWidth.half" :y="sumHeight - 6" />
       <Container v-for="(v, i) in options" :key="i" :y="(bgHeight + 2) * i">
-        <RoundRectangle :fillColor="COLORS.brown" :radius="7" :width="bgWidth" :height="bgHeight" @pointerdown.stop="select($event, i)" />
+        <Image texture="menu_label" :origin="0.5" :x="bgWidth.half" :y="bgHeight.half + 1" :scaleX="(bgWidth + 20) / 112" :scaleY="0.9" @pointerdown.stop="select($event, i)" />
         <Text :ref="v.ref" :originX="0.5" :x="bgWidth.half" :text="v.text" :size="14" color="soy" :y="5" :lineSpacing="3" />
       </Container>
     </Container>
@@ -12,12 +12,12 @@
 </template>
 
 <script>
-import { refObj, Container, RoundRectangle, Image } from 'phavuer'
+import { refObj, Container, Image } from 'phavuer'
 import { reactive, toRefs, onMounted, inject } from 'vue'
 import config from '@/data/config'
 import Text from '@/components/Text'
 export default {
-  components: { Container, RoundRectangle, Image, Text },
+  components: { Container, Image, Text },
   props: ['list'],
   emits: ['select'],
   setup (props, context) {
