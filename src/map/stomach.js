f5f04f7e2f8f96a4a62d8a9f29744506d746dfcd
Laineus <lainen000@gmail.com>
2021-05-02 02:41:10 +0900

    reward item

--- src/map/stomach.js
+++ src/map/stomach.js
@@ -1,10 +1,14 @@
 import { inject } from 'vue'
+import { MAIN_STEPS } from '@/data/eventSteps'
 export default {
   bgm: 'wind',
   async create () {
     const field = inject('field').value
     const state = inject('storage').state
     const uiScene = inject('uiScene').value
+
+    field.getObjectById(8).setVisible(state.events.main === MAIN_STEPS.DEAD)
+    field.getObjectById(9).setVisible(state.events.main === MAIN_STEPS.DEAD)
 
     if ((state.status.heart + state.status.body) < 1) {
       field.getObjectById(4).setEvent(async () => uiScene.log.push(t('events.block.status', 1)))
