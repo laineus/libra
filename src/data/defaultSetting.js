4b919dd79596ff9deafba74a88766e4bfbae78ef
Laineus <lainen000@gmail.com>
2021-05-01 13:37:11 +0900

    default lang

--- src/data/defaultSetting.js
+++ src/data/defaultSetting.js
@@ -1,5 +1,11 @@
+const getDefaultLang = () => {
+  const nav = window.navigator
+  if (nav.languages) return nav.languages[0]
+  return nav.language || nav.userLanguage || nav.browserLanguage
+}
+const lang = getDefaultLang().startsWith('ja') ? 'ja' : 'en'
 export default {
   bgm: 100,
   se: 100,
-  lang: 'ja'
+  lang
 }
