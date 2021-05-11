2d358de1e0295c8daeb97dd0ccdc4b450cebe28b
Laineus <lainen000@gmail.com>
2021-05-11 17:09:29 +0900

    update docs

--- TODO.md
+++ TODO.md
@@ -1,22 +1,5 @@
 
-# やること
-プレスリリース
-LP
-
 # 修正
 iOS音でない
 壁にぶつかったときのガクガク
 一番近いアイテムの手アイコン
-
-# 実績
-- awaken リブラの見た夢 シナリオをクリアしたA
-- rest リブラの見た夢 シナリオをクリアしたB
-- quest コンプリート 全てのクエストを完了した
-- drink 一服 ジュースを買った break time
-- trash 清掃員 ゴミを捨てた garbage collector
-- tissue 積極的 ティッシュを使い切った passionate
-- raptor 考古学者 Archaeologist
-- cosmos 宇宙旅行 Space traveler
-- art 絵画収集家 art collector
-- pink ピンクペンギン pink penguin
-- murder 殺害 murder
--- src/class/AchieveManager.js
+++ src/class/AchieveManager.js
@@ -1,3 +1,14 @@
+// - awaken リブラの見た夢 シナリオをクリアしたA
+// - rest リブラの見た夢 シナリオをクリアしたB
+// - quest コンプリート 全てのクエストを完了した
+// - drink 一服 ジュースを買った break time
+// - trash 清掃員 ゴミを捨てた garbage collector
+// - tissue 積極的 ティッシュを使い切った passionate
+// - raptor 考古学者 Archaeologist
+// - cosmos 宇宙旅行 Space traveler
+// - art 絵画収集家 art collector
+// - pink ピンクペンギン pink penguin
+// - murder 殺害 murder
 const getLocalStorageAchievements = () => {
   try {
     const json = localStorage.getItem('achievements')
