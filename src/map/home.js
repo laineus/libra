fa8fb6a96424eeb357b58618e76d652b062f2b91
Laineus <lainen000@gmail.com>
2021-06-18 11:49:30 +0900

    fix amili's script

--- src/map/home.js
+++ src/map/home.js
@@ -131,8 +131,10 @@ export default {
       return true
     }
     const requestApple = async () => {
-      if ((state.status.heart + state.status.body) === 0) return
-      if (Math.chance(0.15)) return await speakAmiliScripts(t('events.home.iloveyou'))
+      const totalCharm = state.status.heart + state.status.body
+      if (totalCharm === 0) return
+      if (Math.chance(totalCharm >= 10 ? 0.3 : 0.15)) return await speakAmiliScripts(t('events.home.iloveyou'))
+      if (totalCharm >= 13) return
       if (state.events.main === MAIN_STEPS.DEAD) return
       if (bag.hasItem('apple')) return
       await speakAmiliScripts(t(Math.chance(0.5) ? 'events.home.requestApple.a' : 'events.home.requestApple.b'))
