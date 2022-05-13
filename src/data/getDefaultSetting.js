00b95a56c9cacf49aceee6047a1ddec0c8d0e5a7
Laineus <lainen000@gmail.com>
2022-05-13 21:20:31 +0900

    default lang

--- src/data/getDefaultSetting.js
+++ src/data/getDefaultSetting.js
@@ -1,13 +1,19 @@
-const getDefaultLang = () => {
+const getBrowserLang = () => {
   const nav = window.navigator
   if (nav.languages) return nav.languages[0]
   return nav.language || nav.userLanguage || nav.browserLanguage
 }
+const getDefaultLangSetting = () => {
+  const lang = getBrowserLang()
+  if (lang.startsWith('ja')) return 'ja'
+  if (lang.startsWith('zh')) return 'cn'
+  if (lang.startsWith('es')) return 'es'
+  return 'en'
+}
 export default () => {
-  const lang = getDefaultLang().startsWith('ja') ? 'ja' : 'en'
   return {
     bgm: 70,
     se: 100,
-    lang
+    lang: getDefaultLangSetting()
   }
 }
